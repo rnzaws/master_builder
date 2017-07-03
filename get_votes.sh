70f3734dee72b4865391a9d950c09ad5040b88c1
Ryan <ryannitz@amazon.com>
2017-07-03 16:11:51 -0400

    Clean

--- get_votes.sh
+++ get_votes.sh
@@ -4,7 +4,9 @@ URL="$1/prod/votes"
 
 DB_NAME="event-votes-$2"
 
+EVENT_ID="newnewnewtest"
+
 curl -H "Content-Type: application/x-amz-json-1.1" -X POST -d \
-"{ \"ConsistentRead\": false, \"Key\": { \"EventId\" : { \"S\": \"newnewnewtest\" } }, \"ProjectionExpression\": \"NayCount, YeaCount\", \"ReturnConsumedCapacity\": \"TOTAL\", \"TableName\": \"$DB_NAME\" }" $URL
+"{ \"ConsistentRead\": false, \"Key\": { \"EventId\" : { \"S\": \"$EVENT_ID\" } }, \"ProjectionExpression\": \"NayCount, YeaCount\", \"ReturnConsumedCapacity\": \"TOTAL\", \"TableName\": \"$DB_NAME\" }" $URL
 
 
