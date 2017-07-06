ea4fa5b96f00e87a3cd9bee22eec8c898de66fae
Ryan <ryannitz@amazon.com>
2017-07-06 12:34:43 -0400

    Dev

--- app/mobile/MasterBuilder/MasterBuilder/Settings.swift
+++ app/mobile/MasterBuilder/MasterBuilder/Settings.swift
@@ -3,9 +3,9 @@ import Foundation
 
 struct Settings {
     struct Credentials {
-        static let APPLICATION_TYPE_ID = "iOS"
+        static let APPLICATION_TYPE_ID = "iOS-99Test"
         static let CLIENT_ID = "amzn1.application-oa2-client.9f33a4d9d3c74c4bb920eeb36859ac7f"
-        static let DSN = "123"
+        static let DSN = "233423423q22"
         static let SCOPES = ["profile"]
     }
 }
